84a6f025b5131f10cd11b28612066d76405c9c5d
JonathanTGold <jonathan.gold@86@gmail.com>
2022-10-11 16:49:29 +0200

    fix ESLint configuration for tests dir, and fix Unit Testing

--- tests/e2e/plugin-example.spec.js
+++ tests/e2e/plugin-example.spec.js
@@ -1,16 +1,11 @@
 import { expect } from 'chai';
-import { createSandbox, spy } from 'sinon';
 import '../../src/index';
 import { setup } from 'kaltura-player-js';
-import { IconComponent } from '../mock/compenents/icon.component';
 import { config, targetId } from '../mock/config';
 import { mediaData } from '../mock/media-sourc';
-import '../mock/plugins/index';
 
 describe('Upper Bar Manager', () => {
   let player;
-  let upperBarManagerService;
-  const sandbox = createSandbox();
 
   before(() => {
     const element = document.createElement('DIV');
@@ -27,21 +22,25 @@ describe('Upper Bar Manager', () => {
     for (const element of document.getElementsByTagName('video')) {
       element.remove();
     }
-    sandbox.restore();
   });
 
-  it('Add icon', async () => {
+  it('Plugin configured properly', async () => {
     // Given
-    player = setup(config);
-    upperBarManagerService = player.getService('upperBarManager');
+    player = setup({
+      ...config,
+      plugins: {
+        pluginExample: {
+          developerName: 'Stiven Hoking'
+        }
+      }
+    });
+    const pluginInstance = player.plugins.pluginExample;
     player.setMedia({ sources: { ...mediaData } });
     await player.ready();
 
     // Do
-    const iconId = upperBarManagerService.add({ label: 'icon', component: IconComponent, onClick: () => {} });
 
     // Expect
-    expect(upperBarManagerService.componentsRegistry.size).to.equal(1);
-    expect(upperBarManagerService.componentsRegistry.get(iconId).label).equal('icon');
+    expect(pluginInstance.config.developerName).equal('Stiven Hoking');
   });
 });

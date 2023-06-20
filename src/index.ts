2e8b9ba1fd072073d08166b5199c04ac1ebae844
JonathanTGold <62672270+JonathanTGold@users.noreply.github.com>
2023-06-20 12:17:31 +0300

    build(FEC-13123): replace Travis with GitHub actions (#3)
    replace Travis with GitHub actions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { registerPlugin } from 'kaltura-player-js';
+import { registerPlugin } from '@playkit-js/kaltura-player-js';
 import { pluginName, PluginExample } from './plugin-example';
 
 registerPlugin(pluginName, PluginExample);

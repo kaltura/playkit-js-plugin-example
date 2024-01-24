244aa9a69f4baebfdb6ea5145f82d053f5b017ca
JonathanTGold <jonathan.gold@86@gmail.com>
2024-01-24 09:46:44 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { registerPlugin } from '@playkit-js/kaltura-player-js';
 import { pluginName, PluginExample } from './plugin-example';
-import { PluginExampleConfig } from './types';
 
 registerPlugin(pluginName, PluginExample);
 
